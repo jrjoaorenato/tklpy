04c0ea140065bf7fc4f5779dcf935c153091698d
jrjoaorenato <joaorenatorm@gmail.com>
2020-05-30 15:33:58 -0300

    kernel function implemented

--- tklpy/tkl.py
+++ tklpy/tkl.py
@@ -1,8 +1,8 @@
-from tklpy import utils
+from tklpy.utils import kernelTypes
 
 class TKL:
     """ TKL calculations class """
-    def __init__(self, Xs, Xt, Ys, Yt, ker = rbf, gamma = 1.0, eta = 2.0):
+    def __init__(self, Xs, Xt, Ys, Yt, ker = rbf, gamma = 1.0, eta = 1.1):
         """Constructore of the TKL class
 
         Arguments:
@@ -12,10 +12,31 @@ class TKL:
             Xt {Matrix [ntxf]} -- [Target domain data]
             Ys {Array with ns elements} -- [Labels of the Source Domain data]
             Yt {Array with nt elements} -- [Labels of the Target Domain data, for benchmarking purposes]
+            ker is the type of kernel present utils.kernelTypes
+                rbf = 0 - is the rbf kernel
+                linear = 1 - is the linear kernel
+                lap = 2 - is the lap kernel - todo
+            gamma reffers to the gamma proprierty of the rbf kernel
+            eta - ?
+                
         """
         self.Xs = Xs
         self.Xt = Xt
         self.Ys = Ys
         self.Yt = Yt
+        self.ker = ker
+        self.gamma = gamma
+        self.eta = eta
     
-    # def findTKL
+    def findTKL():
+        pass
+    def findKernel():
+        pass
+    def returnTrainData():
+        pass
+    def returnTestData():
+        pass
+    def trainSVM():
+        pass
+    def testSVM():
+        pass
--- tklpy/utils.py
+++ tklpy/utils.py
@@ -1,7 +1,21 @@
 #calculate kernel with sklearn
 from enum import Enum
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics.pairwise import linear_kernel
 
-class KernelEnum(Enum):
+class kernelTypes(Enum):
     rbf = 0
     linear = 1
-    lap = 2
+#    lap = 2 #todo
+    
+class kernelFinder:
+    def findKernel(X1, X2, kernelType, gamma):
+        if (kernelType == kernelTypes.rbf):
+            return rbf_kernel(X1, X2, gamma)
+        elif (kernelType == kernelTypes.linear):
+            return linear_kernel(X1, X2)
+    #    elif (kernelType == kernelTypes.lap):
+    #        pass
+        else:
+            raise NameError('Unsupported Kernel')
+
