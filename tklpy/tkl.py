bf65ee570ab8add4344d4995af97779f787e20c8
jrjoaorenato <joaorenatorm@gmail.com>
2020-05-31 14:25:46 -0300

    comments added

--- tklpy/tkl.py
+++ tklpy/tkl.py
@@ -16,8 +16,8 @@ class TKL:
         Arguments:
             n is the number of instances
             and f is the dimensionality of the features
-            Xs {numpy Matrix [nsxf]} -- [Source domain data]
-            Xt {numpy Matrix [ntxf]} -- [Target domain data]
+            Xs {numpy Matrix [NS x F]} -- [Source domain data]
+            Xt {numpy Matrix [NT x F]} -- [Target domain data]
             Ys {numpy Array with ns elements} -- [Labels of the Source Domain data]
             Yt {numpy Array with nt elements} -- [Labels of the Target Domain data, for benchmarking purposes]
             ker is the type of kernel present utils.kernelTypes
@@ -37,11 +37,17 @@ class TKL:
         self.eta = eta
     
     def findTKL(self):
+        """ This function is used to find the TKL Kernel 
+        that can be used to feed an SVM
+
+        Returns:
+            Matrix -- The Domain Invariant Kernel Matrix
+        """
         X1 = (self.Xs).transpose()
         X2 = (self.Xt).transpose()
         X = np.concatenate((X1,X2),axis=1)
         m = X1.shape[1]
-        K = kernelFinder.findKernel(X, self.kerType, self.gamma)
+        K = kernelFinder.findKernel(X.transpose(), self.kerType, self.gamma)
         K = K + 1e-6 * np.eye(K.shape[1])
         Ks = K[0:m, 0:m]
         Kt = K[m:,m:]
@@ -70,53 +76,119 @@ class TKL:
         return self.tkl
 
     def findKernel(self):
+        """Find the Kernel with the data and parameters
+        specified in the constructor for TKL comparison
+
+        Returns:
+            Matrix -- Kernel specified in the constructor
+        """
         X1 = (self.Xs).transpose()
         X2 = (self.Xt).transpose()
         X = np.concatenate((X1,X2),axis=1)
-        self.Kernel = kernelFinder.findKernel(X, self.kerType, self.gamma)
+        self.Kernel = kernelFinder.findKernel(X.transpose(), self.kerType, self.gamma)
         return self.Kernel
 
     def returnSourceDataTKL(self):
+        """Returns the  instances of the Source Data
+        projected in the Domain Invariant Kernel
+        Matrix for training the algorithm
+
+        Returns:
+            Matrix -- Source Data on the Domain Invariant Kernel
+        """
         #used to train the svm
         m = (self.Xs).transpose().shape[1]
         return self.tkl[0:m,0:m]
         
     def returnTargetDataTKL(self):
+        """Returns the  instances of the Target Data
+        projected in the Domain Invariant Kernel
+        Matrix for testing the algorithm
+
+        Returns:
+            Matrix -- Target Data on the Domain Invariant Kernel
+        """
         #used to test the svm
         m = (self.Xs).transpose().shape[1]
         return self.tkl[m:,0:m]
 
     def returnSourceDataKernel(self):
+        """Returns the  instances of the Source Data
+        projected in the original Kernel training
+        the algorithm
+
+        Returns:
+            Matrix -- Source Data on the Kernel
+        """
         m = (self.Xs).transpose().shape[1]
         return self.Kernel[0:m,0:m]
 
     def returnTargetDataKernel(self):
+        """Returns the  instances of the Target Data
+        projected in the original Kernel training
+        the algorithm
+
+        In this case for fair comparison, the data
+        extracted is of the Target Data within the
+        Source Data Kernel for Kernel transference
+        analysis
+
+        Returns:
+            Matrix -- Target Data on the Kernel
+        """
         m = (self.Xs).transpose().shape[1]
         return self.Kernel[m:,0:m]
 
     def trainSVMforTKL(self):
+        """Trains a Support Vector Machine based on 
+        libsvm with the data obtained from the method
+        'returnSourceDataTKL()'
+        """
         Xs = self.returnSourceDataTKL()
         TKsvc = SVC(kernel='precomputed')
         TKsvc.fit(Xs, self.Ys)
         self.TKsvc = TKsvc
 
     def testSVMforTKL(self):
+        """Tests the Support Vector Machine obtained
+        from the method 'trainSVMforTKL()' the data
+        obtained from the method 'returnTargetDataTKL()'
+        """
         Xt = self.returnTargetDataTKL()
         TKy_pred = self.TKsvc.predict(Xt)
         acc_tkl = accuracy_score(self.Yt, TKy_pred)
         return acc_tkl
     
     def trainSVMforKernel(self):
+        """Trains a Support Vector Machine based on 
+        libsvm with the data obtained from the method
+        'returnSourceDataKernel()'
+        """
         Xs = self.returnSourceDataKernel()
         TKsvc = SVC(kernel='precomputed')
         TKsvc.fit(Xs, self.Ys)
         self.TKsvc = TKsvc
 
     def testSVMforKernel(self):
+        """Tests the Support Vector Machine obtained
+        from the method 'trainSVMforKernel()' the data
+        obtained from the method 'returnTargetDataKernel()'
+        """
         Xt = self.returnTargetDataKernel()
         TKy_pred = self.TKsvc.predict(Xt)
         acc_tkl = accuracy_score(self.Yt, TKy_pred)
         return acc_tkl
 
     def convertDatatoTKL(self, X):
+        """Receives a data input X and projects it 
+        into the Domain Invariant Kernel Matrix,
+        using this case we can use different instances
+        of data for testing
+
+        Arguments:
+            X {Data Matrix [Nd x f]} -- Data to be projected
+            into the Domain Invariant Kernel, in this case,
+            Nd is the number of samples and f is the 
+            dimensionality of the features
+        """
         pass
--- tklpy/utils.py
+++ tklpy/utils.py
@@ -4,17 +4,34 @@ from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.metrics.pairwise import linear_kernel
 
 class kernelTypes(Enum):
+    """Enum Class to define the types of Kernel
+
+    Arguments:
+        Enum {kernel - int} -- each number referes to
+        a type of kernel
+    """
     rbf = 0
     linear = 1
     rbfLang = 2
 #    lap = 2 #todo
     
 class kernelFinder:
-    def findKernel(self, X, kernelType, gammaVar):
+    def findKernel(self, X, kernelType, gammaVar = 1.0):
+        """Returns the kernel based on the type of 
+        enum kernel passed
+
+        Arguments:
+            X {Matrix} -- Data to be kernelized
+            kernelType {kernelTypes enum} -- Kernel choosen
+            gammaVar {float} -- gamma variable on rbf kernel
+
+        Returns:
+            [Matrix] -- Kernelized data
+        """
         if (kernelType == kernelTypes.rbf):
-            return rbf_kernel(X.transpose(), gamma=gammaVar)
+            return rbf_kernel(X, gamma=gammaVar)
         elif (kernelType == kernelTypes.linear):
-            return linear_kernel(X.transpose(), dense_output=False)
+            return linear_kernel(X, dense_output=False)
     #    elif (kernelType == kernelTypes.lap):
     #        pass
         elif (kernelType == kernelTypes.rbfLang):
