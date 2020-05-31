83350671edcd13ddfcf27365bae1987e48483f13
jrjoaorenato <joaorenatorm@gmail.com>
2020-05-30 21:19:47 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+import setuptools
 
-setup(name='tklpy',
-      version='0.1',
+setuptools.setup(name='tklpy',
+      version='0.0.1',
       description='Python implementation of the Transfer Kernel Learning method developed by Long et. al. (2015).',
       url='http://github.com/jrjoaorenato/tklpy',
       author='João Renato Ribeiro Manesco',
       author_email='joaorenatorm@gmail.com',
+      description="A small example package",
       license='MIT',
-      packages=['tklpy'],
+      packages=setuptools.find_packages(),
       zip_safe=False)
