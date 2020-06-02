7c676a7e68eab86b869b15899bcef69735e3f375
João Renato Ribeiro Manesco <joaorenatorm@gmail.com>
2020-06-02 00:55:49 -0300

    Update tkl.py

--- tklpy/tkl.py
+++ tklpy/tkl.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score
 
 class TKL:
     """ TKL calculations class uses numpy data structures"""
-    def __init__(self, Xs, Xt, Ys, Yt, ker = kernelTypes.rbf, gamma = 1.0, eta = 1.1):
+    def __init__(self, Xs, Xt, Ys, Yt, ker = kernelTypes.rbfLang, gamma = 1.0, eta = 1.1):
         """Constructor of the TKL class
 
         Arguments:
@@ -21,9 +21,9 @@ class TKL:
             Ys {numpy Array with ns elements} -- [Labels of the Source Domain data]
             Yt {numpy Array with nt elements} -- [Labels of the Target Domain data, for benchmarking purposes]
             ker is the type of kernel present utils.kernelTypes
-                rbf = 0 - is the rbf kernel
+                rbf = 0 - is the RBF kernel
                 linear = 1 - is the linear kernel
-                lap = 2 - is the lap kernel - todo
+                rbfLang = 2 - RBF Kernel using the original authors implementation and gamma calculation
             gamma reffers to the gamma proprierty of the rbf kernel
             eta is the eigenspectrum damping factor
                 
