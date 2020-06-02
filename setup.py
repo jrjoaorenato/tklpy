d0eb701684d3b5f301796c7fca8be7168fa1221c
jrjoaorenato <joaorenatorm@gmail.com>
2020-06-01 21:02:01 -0300

    setup files

--- setup.py
+++ setup.py
@@ -7,5 +7,21 @@ setuptools.setup(name='tklpy',
       author='João Renato Ribeiro Manesco',
       author_email='joaorenatorm@gmail.com',
       license='MIT',
-      packages=setuptools.find_packages(),
-      zip_safe=False)
+      packages=['tklpy'],
+      keywords = ['Domain Adaptation', 'Machine Learning', 'TKL', 'Domain Invariant Transfer Kernel Learning'],
+      install_requires=[            # I get to this in a second
+            'numpy',
+            'qpsolvers',
+            'quadprog',
+            'scikit-learn',
+            'scipy'
+      ],
+      classifiers=[
+            'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Intended Audience :: Developers',      # Define that your audience are developers
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',   # Again, pick a license
+            'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+            'Programming Language :: Python :: 3.8',
+      ],
+      )
